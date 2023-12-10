87cf784ec12b9a0899aa20d8bf5db8fae851522a
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-10 14:05:22 -0600

    Made orange bar

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ let dataArray = [4, 15, 34];
 
 const svg = canvas.append("svg").attr("width", 600).attr("height", 600);
 
-const rect = svg.append("Rect");
+const rect = svg.append("rect");
+
+rect.attr("width", 24).attr("height", 100).attr("fill", "orange");
