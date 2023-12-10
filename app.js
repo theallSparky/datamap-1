8a04a23b5c374ce9312c7251335da63e34720ed7
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-10 15:08:10 -0600

    moved y axis, added comment clarification

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ rect
   .data(dataArray)
   .attr("height", 100)
   .attr("fill", "orange")
-  //d is element in array
+  //d is element in array, i is index
+  .attr("y", function (d, i) {
+    return d * 12;
+  })
   .attr("x", function (d, i) {
     return d * 20;
   });
