b8112fe9e85d71a487fec05041e2a0f606c656c1
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-10 22:09:22 -0600

    Dynamically rendered bars via array data

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ const canvas = d3.select(".canva");
 
 let dataArray = [4, 15, 34];
 
-const svg = canvas.append("svg").attr("width", 600).attr("height", 600);
+// const svg = canvas.append("svg").attr("width", 600).attr("height", 600);
 
-const rect = svg.append("rect");
+const svg = canvas.select("svg");
+
+// const rect = svg.append("rect");
+const rect = svg.selectAll("rect");
 
 rect
   .attr("width", 24)
   .data(dataArray)
-  .attr("height", 100)
   .attr("fill", "orange")
-  //d is element in array, i is index
-  .attr("y", function (d, i) {
-    return d * 12;
+  .attr("height", function (d) {
+    return d * 2;
   })
   .attr("x", function (d, i) {
-    return d * 20;
+    return i * 25;
   });
