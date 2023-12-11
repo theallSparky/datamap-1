851a1e488c9973538b09b927ff8c1b0a5b60ac2a
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-10 22:46:32 -0600

    Changed the bar graph a bit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const canvas = d3.select(".canva");
 
-let dataArray = [4, 15, 34];
+let dataArray = [4, 25, 34];
 
 // const svg = canvas.append("svg").attr("width", 600).attr("height", 600);
 
@@ -14,8 +14,11 @@ rect
   .data(dataArray)
   .attr("fill", "orange")
   .attr("height", function (d) {
-    return d * 2;
+    return d * 3;
   })
   .attr("x", function (d, i) {
     return i * 25;
+  })
+  .attr("y", function (d, i) {
+    return 100 - d * 3;
   });
