74ab0b7f45996eb3f7301f27fef2fa509de7240a
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-12 09:53:45 -0600

    Made graph using text.json file

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const canvas = d3.select(".canva");
 
-let dataArray = [
-  { width: 25, height: 4, fill: "pink" },
-  { width: 25, height: 14, fill: "purple" },
-  { width: 25, height: 44, fill: "orange" },
-  { width: 25, height: 124, fill: "green" },
-  { width: 25, height: 12, fill: "gray" },
-  { width: 25, height: 32, fill: "red" },
-];
+// let dataArray = [
+//   { width: 25, height: 4, fill: "pink" },
+//   { width: 25, height: 14, fill: "purple" },
+//   { width: 25, height: 44, fill: "orange" },
+//   { width: 25, height: 124, fill: "green" },
+//   { width: 25, height: 12, fill: "gray" },
+//   { width: 25, height: 32, fill: "red" },
+// ];
 
 const svg = canvas.append("svg").attr("width", 600).attr("height", 600);
 
@@ -15,22 +15,21 @@ const rect = svg.selectAll("rect");
 
 d3.json("text.json").then((data) => {
   console.log(data);
+  rect
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("width", 24)
+    .attr("fill", function (d) {
+      return d.fill;
+    })
+    .attr("height", function (d) {
+      return d.height * 2;
+    })
+    .attr("x", function (d, i) {
+      return i * 25;
+    })
+    .attr("y", function (d, i) {
+      return 100 - d.height * 2;
+    });
 });
-
-rect
-  .data(dataArray)
-  .enter()
-  .append("rect")
-  .attr("width", 24)
-  .attr("fill", function (d) {
-    return d.fill;
-  })
-  .attr("height", function (d) {
-    return d.height * 2;
-  })
-  .attr("x", function (d, i) {
-    return i * 25;
-  })
-  .attr("y", function (d, i) {
-    return 100 - d.height * 2;
-  });
